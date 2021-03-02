4e79581371092a67d2c76969cbfd816e3a087908
yogeshwaran <yogeshcse45@gmail.com>
2021-03-02 21:24:02 +0530

    Updates

---
+++ precache-manifest.8305e9b15cf62b149211e4c0e5f6e582.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a93fe2d64bff3a131bfc20d3f9f5fa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "54caf5cfea6da6533e25",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "e7907d7f2d2e4df773c7",
+    "url": "./static/js/2.6770e0a6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.6770e0a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54caf5cfea6da6533e25",
+    "url": "./static/js/main.04efab09.chunk.js"
+  },
+  {
+    "revision": "bcd969de8b1cc9098084",
+    "url": "./static/js/runtime-main.0b52047f.js"
+  }
+]);
